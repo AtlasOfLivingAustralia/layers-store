6ab75e7388b74c0466727a21b1a8a1ed560fe431
adam-collins <adam.collins832@gmail.com>
2014-08-08 18:43:57 +1000

    - fix user data sampling

--- src/main/java/org/ala/layers/util/Occurrences.java
+++ src/main/java/org/ala/layers/util/Occurrences.java
@@ -4,11 +4,9 @@ import au.com.bytecode.opencsv.CSVReader;
 import org.ala.layers.intersect.SimpleRegion;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.util.ByteArrayBuilder;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -16,6 +14,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Created by a on 10/03/2014.
@@ -38,37 +38,66 @@ public class Occurrences {
 
         StringBuilder output = new StringBuilder();
 
-        while (true && start < 300000000) {
-            String url = server + "/webportal/occurrences.gz?q=" + q + "&pageSize=" + pageSize + "&start=" + start + "&fl=" + fields;
-            logger.info("retrieving from biocache : " + url);
-            int tryCount = 0;
-            InputStream is = null;
-            String csv = null;
-            int maxTrys = 4;
-            while (tryCount < maxTrys && csv == null) {
-                tryCount++;
-                try {
-                    is = getUrlStream(url);
-                    csv = IOUtils.toString(new GZIPInputStream(is));
-                } catch (Exception e) {
-                    logger.warn("failed try " + tryCount + " of " + maxTrys + ": " + url, e);
+        //TODO: use info from the original analysis request instead of doing this
+        boolean isUserData = server.contains("spatial");
+
+        if (isUserData) {
+            String url = server + "/userdata/sample?q=" + q + "&fl=" + fields;
+            logger.info("getting occurrences from : " + url);
+            try {
+                InputStream is = getUrlStream(url);
+                ZipInputStream zis = new ZipInputStream(is);
+                ZipEntry ze = zis.getNextEntry();
+
+                ByteArrayBuilder bab = new ByteArrayBuilder();
+                byte [] b = new byte[1024];
+                int n;
+                while ((n = zis.read(b,0,1024)) > 0) {
+                    bab.write(b,0,n);
                 }
+
+                String csv = IOUtils.toString(bab.toByteArray(), "UTF-8");
+
+                is.close();
+
+                output.append(csv);
+            } catch (Exception e) {
+                logger.error("failed to get userdata as csv for url: " + url, e);
             }
 
-            if (csv == null) {
-                throw new IOException("failed to get records from biocache.");
-            }
+        } else {
+            while (start < 300000000) {
+                String url = server + "/webportal/occurrences.gz?q=" + q + "&pageSize=" + pageSize + "&start=" + start + "&fl=" + fields;
+                logger.info("retrieving from biocache : " + url);
+                int tryCount = 0;
+                InputStream is = null;
+                String csv = null;
+                int maxTrys = 4;
+                while (tryCount < maxTrys && csv == null) {
+                    tryCount++;
+                    try {
+                        is = getUrlStream(url);
+                        csv = IOUtils.toString(new GZIPInputStream(is));
+                    } catch (Exception e) {
+                        logger.warn("failed try " + tryCount + " of " + maxTrys + ": " + url, e);
+                    }
+                }
 
-            output.append(csv);
+                if (csv == null) {
+                    throw new IOException("failed to get records from biocache.");
+                }
 
-            int currentCount = 0;
-            int i = -1;
-            while ((i = csv.indexOf("\n", i + 1)) > 0) {
-                currentCount++;
-            }
+                output.append(csv);
 
-            if (currentCount == 0 || currentCount < pageSize) {
-                break;
+                int currentCount = 0;
+                int i = -1;
+                while ((i = csv.indexOf("\n", i + 1)) > 0) {
+                    currentCount++;
+                }
+
+                if (currentCount == 0 || currentCount < pageSize) {
+                    break;
+                }
             }
         }
 
